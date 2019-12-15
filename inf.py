b0641e79d604125507404434f35ded35ab054205
danielzhe <danielzhe@yandex-team.ru>
2019-12-15 22:16:47 +0500

    Cleaned the project

--- inf.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-os.environ['OUT_FILE_NAME'] = '123'
-print(os.environ)
-
-while True:
-    pass
--- print_port.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-import sys, serial
-port = serial.Serial(sys.argv[1])
-print(port)
-port.close()
